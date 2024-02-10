1eae71e0ff47ad69e9f3073e532eda0752ebec09
RobbieS-coder <127651345+RobbieS-coder@users.noreply.github.com>
2024-02-10 16:00:49 +0000

    Add size method

--- lib/main.rb
+++ lib/main.rb
@@ -25,19 +25,37 @@ class LinkedList
     @head = Node.new(item, @head)
   end
 
+  def size
+    current = @head
+    counter = 1
+
+    until last_node?(current)
+      counter += 1
+      current = current.next_node
+    end
+
+    counter
+  end
+
   private
 
-  def tail(node = @head)
-    return node if node.next_node.nil?
+  def tail
+    node = @head
 
-    tail(node.next_node)
+    node = node.next_node until last_node?(node)
+
+    node
+  end
+
+  def start_list(item)
+    @head = Node.new(item)
   end
 
   def empty_list?
     head.nil?
   end
 
-  def start_list(item)
-    @head = Node.new(item)
+  def last_node?(node)
+    node.next_node.nil?
   end
 end
